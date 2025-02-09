075b63d0b7c9b16a002fdabe2c807d0b53dbeb3c
@RecoveryIsBusyYouNeedFormatData <walaup99@gmail.com>
2025-02-09 13:50:06 +0700

    Update gpucc-sdm845.c

--- HardTeam/OC/840/gpucc-sdm845.c
+++ HardTeam/OC/840/gpucc-sdm845.c
@@ -279,9 +279,9 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(342000000, P_CRC_DIV,  1, 0, 0),
 	F(414000000, P_CRC_DIV,  1, 0, 0),
 	F(596000000, P_CRC_DIV,  1, 0, 0),
+	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
-	F(800000000, P_CRC_DIV,  1, 0, 0),
-	F(810000000, P_CRC_DIV,  1, 0, 0),
+	F(827000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -611,7 +611,7 @@ static void gpu_cc_sdm845_fixup_sdm845v2(struct regmap *regmap)
 	clk_fabia_pll_configure(&gpu_cc_pll1, regmap, &gpu_cc_pll1_config);
 
 	gpu_cc_gmu_clk_src.freq_tbl = ftbl_gpu_cc_gmu_clk_src_sdm845_v2;
-	gpu_cc_gmu_clk_src.clkr.hw.init->rate_max[VDD_CX_LOW] = 800000000;
+	gpu_cc_gmu_clk_src.clkr.hw.init->rate_max[VDD_CX_LOW] = 827000000;
 }
 
 static void gpu_cc_sdm845_fixup_sdm670(struct regmap *regmap)
@@ -636,10 +636,10 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL] =
 		596000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
-		710000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 800000000;
+		675000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 710000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		810000000;	;
+		827000000;	;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
